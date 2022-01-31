24b55d4f1fb6abb50d6991f5edf0118ed46e79dc
lombable <rinconaedgar@gmail.com>
2022-01-31 12:43:49 -0300

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let randomsymbol = Math.floor(Math.random() * symbol.length);
+  let randomnumber = Math.floor(Math.random() * number.length);
+
+  document.querySelector(".top").innerHTML = symbol[randomsymbol];
+  document.querySelector(".number").innerHTML = number[randomnumber];
+  document.querySelector(".bottom").innerHTML = symbol[randomsymbol];
+
+  let color = () => {
+    if (symbol[randomsymbol] == "♥" || symbol[randomsymbol] == "♦") {
+      document.querySelector(".top").style.color = "red";
+      document.querySelector(".number").style.color = "red";
+      document.querySelector(".bottom").style.color = "red";
+    }
+  };
+  color();
 };
